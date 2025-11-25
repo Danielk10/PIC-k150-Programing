dd06057ffa1675238b6d98d0e9e6905717a1b6cb
Danielk10 <danielpdiamon@gmail.com>
2025-11-25 14:31:59 -0400

    feat: Add privacy policy and GPUTILS tutorial activities with full-screen utility and theme configuration.

--- app/src/main/java/com/diamon/politicas/Politicas.java
+++ app/src/main/java/com/diamon/politicas/Politicas.java
@@ -6,6 +6,12 @@ import android.webkit.WebView;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.diamon.utilidades.PantallaCompleta;
 
 public class Politicas extends AppCompatActivity {
 
@@ -18,14 +24,17 @@ public class Politicas extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Configurar edge-to-edge para Android 15+
+        PantallaCompleta pantallaCompleta = new PantallaCompleta(this);
+        pantallaCompleta.habilitarEdgeToEdge();
+
         diseno = new LinearLayout(this);
-        
+
         diseno.setOrientation(LinearLayout.VERTICAL);
 
-        LinearLayout.LayoutParams parametros =
-                new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
 
         pagina = new WebView(this);
 
@@ -33,8 +42,12 @@ public class Politicas extends AppCompatActivity {
 
         setContentView(diseno);
 
+        // Aplicar window insets al layout para evitar que se oculte contenido
+        pantallaCompleta.aplicarWindowInsets(diseno);
+
         pagina.getSettings().setJavaScriptEnabled(true);
 
-        pagina.loadUrl("https://politicasdeprivacidaspickprograming.blogspot.com/2024/12/pic-k150-programming.html?m=1");
+        pagina.loadUrl(
+                "https://politicasdeprivacidaspickprograming.blogspot.com/2024/12/pic-k150-programming.html?m=1");
     }
 }
